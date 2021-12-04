9db2f851a85105710a91ed8f9fb035de39715fab
ewdlop <ray810815@gmail.com>
2021-12-04 00:52:30 -0500

    changes

--- WaveEngineVulkanWindowApp/Program.cs
+++ WaveEngineVulkanWindowApp/Program.cs
@@ -1,5 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using WaveEngineDotNetLibrary;
+using System.Windows.Forms;
+using System.Diagnostics;
 
-VkContext vkContext = new VkContext();
+const uint WIDTH = 800;
+const uint HEIGHT = 600;
+
+Form window = new Form()
+{
+    Text = "Vulkan Triangle Rasterization",
+    Size = new System.Drawing.Size((int)WIDTH, (int)HEIGHT),
+    FormBorderStyle = FormBorderStyle.FixedToolWindow
+};
+window.Show();
+IntPtr hInstance = Process.GetCurrentProcess().Handle;
+VkContext vkContext = new VkContext(window.Handle, hInstance);
 vkContext.CreateInstance();
